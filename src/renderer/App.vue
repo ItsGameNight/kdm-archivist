952df282c9982929a229b267d9d6790e46535795
Alexander Karle <akarle@umass.edu>
2018-04-19 18:18:59 -0400

    Switching to use electron-vue setup (url below)
    https://github.com/SimulatedGREG/electron-vue

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'kd-monster'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
