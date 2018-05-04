3115cb0c2795e7269510743c3671bf732bcab7d7
Makenzie Schwartz <mawaschwa@gmail.com>
2018-05-04 17:36:36 -0400

    Updated Electron version, more survivor table work
    Electron updated to 1.8. Starting styling survivor table rows.

--- src/renderer/components/SurvivorTable.vue
+++ src/renderer/components/SurvivorTable.vue
@@ -1,43 +1,100 @@
 <template>
-  <div>
-    <survivor-table-row v-for="surv in survivors" :survivor="surv"></survivor-table-row>
-  </div>
+  <table>
+    <tr v-for="surv in survivors"><survivor-table-row :survivor="surv" :key='surv._id'></survivor-table-row></tr>
+  </table>
 </template>
 
 <script>
-  import SurvivorTableRow from './SurvivorTableRow'
+import SurvivorTableRow from './SurvivorTableRow'
 
-  export default {
-    name: 'survivor-table',
-    components: { SurvivorTableRow },
-    data () {
-      return {
-        survivors: [
-          {
-            'name': 'Olympia',
-            'stats': [
-              { 'title': 'strength', 'value': 5 },
-              { 'title': 'accuracy', 'value': 9 },
-              { 'title': 'evasion', 'value': 32 },
-              { 'title': 'speed', 'value': -5 },
-              { 'title': 'speed', 'value': -5 }
-            ]
-          },
-          {
-            'name': 'Swamp Ass',
-            'stats': [
-              { 'title': 'strength', 'value': 10 },
-              { 'title': 'accuracy', 'value': -34 },
-              { 'title': 'evasion', 'value': -69 },
-              { 'title': 'luck', 'value': -13 },
-              { 'title': 'speed', 'value': -5 }
-            ]
-          }
-        ]
-      }
+export default {
+  name: 'survivor-table',
+  components: { SurvivorTableRow },
+  data () {
+    return {
+      survivors: [
+        {
+          '_id': 1,
+          'name': 'Olympia',
+          'sex': 'f',
+          'survival': 4,
+          'movement': 5,
+          'strength': 3,
+          'accuracy': 4,
+          'evasion': 2,
+          'luck': 3,
+          'speed': 1,
+          'insanity': 6,
+          'xp': 4,
+          'courage': 5,
+          'boldSkill': 'Prepared',
+          'understanding': 4,
+          'insightSkill': 'Tinker',
+          'fightingArts': [ 'Mighty Strike', 'Last Man Standing' ],
+          'disorders': [],
+          'abilities': [],
+          'impairments': [],
+          'nickname': 'Wise Fool of the Lantern'
+        },
+        {
+          '_id': 2,
+          'name': 'Swamp Ass',
+          'sex': 'm',
+          'survival': 0,
+          'movement': 4,
+          'strength': 10,
+          'accuracy': -3,
+          'evasion': -6,
+          'luck': 0,
+          'speed': 0,
+          'insanity': 13,
+          'xp': 1,
+          'courage': 0,
+          'boldSkill': null,
+          'understanding': 0,
+          'insightSkill': null,
+          'fightingArts': [],
+          'disorders': [],
+          'abilities': [],
+          'impairments': [],
+          'nickname': null
+        },
+        {
+          '_id': 3,
+          'name': 'Nugget',
+          'sex': 'm',
+          'survival': 3,
+          'movement': 5,
+          'strength': 0,
+          'accuracy': 0,
+          'evasion': 0,
+          'luck': 0,
+          'speed': 0,
+          'insanity': 0,
+          'xp': 0,
+          'courage': 0,
+          'boldSkill': null,
+          'understanding': 0,
+          'insightSkill': null,
+          'fightingArts': [],
+          'disorders': [],
+          'abilities': [],
+          'impairments': [],
+          'nickname': null
+        }
+      ]
     }
   }
+}
 </script>
 
 <style>
+table {
+  width: 100%;
+  border-spacing: 0em 0.5em;
+}
+td {
+  border: 1px solid black;
+  padding: 5px;
+}
 </style>
--- src/renderer/components/SurvivorTableRow.vue
+++ src/renderer/components/SurvivorTableRow.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
-    <h3>{{ survivor.name }}</h3>
-    <ul><li v-for="stat in survivor.stats">{{ stat.title }} : {{ stat.value }}</li></ul>
-  </div>
+  <td>
+    {{ survivor.name }}
+    <span v-for="n in survivor.xp">•</span><span v-for="n in 16 - survivor.xp">○</span>
+    Survival: {{ survivor.survival }}
+    Mov: {{ survivor.movement }}
+    Str: {{ survivor.strength }}
+    Acc: {{ survivor.accuracy }}
+    Eva: {{ survivor.evasion }}
+    Luk: {{ survivor.luck }}
+    Spd: {{ survivor.speed }}
+  </td>
 </template>
 
 <script>
-  export default {
-    name: 'survivor-table-row',
-    props: [
-      'survivor'
-    ]
-  }
+export default {
+  name: 'survivor-table-row',
+  props: [
+    'survivor'
+  ]
+}
 </script>
+
+<style>
+</style>
