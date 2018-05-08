73d17d220d76e4c3c96e1069463e6b79c310d87b
Alexander Karle <akarle@umass.edu>
2018-05-08 16:14:39 -0400

    More legible settlement rows (show name not ID)

--- src/renderer/components/SettlementTableRow.vue
+++ src/renderer/components/SettlementTableRow.vue
@@ -1,6 +1,7 @@
 <template>
   <td @click.stop="setCurrentSmt(settlement._id)" :class="[selected ? 'selected' : '']">
-    <span>{{ settlement._id }}</span>
+    <span v-if="settlement.name !== null">settlement.name</span>
+    <span v-else>NAME ME!</span>
   </td>
 </template>
 
