70d3a6e58d369357b19a723966a35f6851c19a8c
Alexander Karle <akarle@umass.edu>
2018-05-09 16:42:44 -0400

    Starting a vuex store

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import modules from './modules'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules,
+  state: {
+    survivors: [],
+    settlements: [],
+    currentSmt: null
+  },
+  getters: {
+    SET_SURVIVORS (state, newObj) {
+      state.survivors = newObj
+    },
+    SET_SETTLEMENTS (state, newObj) {
+      state.settlements = newObj
+    },
+    SET_CURRENTSMT (state, id) {
+      state.currentSmt = id
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
   strict: process.env.NODE_ENV !== 'production'
 })
