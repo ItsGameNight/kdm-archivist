ae66463baaa04754e96374a1fd63879997846720
Makenzie Schwartz <mawaschwa@gmail.com>
2018-05-21 21:30:54 -0400

    added checked prop to timeline

--- src/renderer/assets/StaticGameData/Timelines/BaseTimeline.js
+++ src/renderer/assets/StaticGameData/Timelines/BaseTimeline.js
@@ -4,121 +4,141 @@ export default {
       number: 1,
       settlementEvent: 'First Day',
       story: 'Returning Survivors',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 2,
       settlementEvent: '',
       story: 'Endless Screams',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 3,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 4,
       settlementEvent: '',
       story: '',
-      nemesis: 'Butcher Lvl 1'
+      nemesis: 'Butcher Lvl 1',
+      checked: false
     },
     {
       number: 5,
       settlementEvent: '',
       story: 'Hands of Heat',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 6,
       settlementEvent: '',
       story: 'Armored Strangers',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 7,
       settlementEvent: '',
       story: 'Phoenix Feather',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 8,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 9,
       settlementEvent: '',
       story: '',
-      nemesis: 'King\'s Man Lvl 1'
+      nemesis: 'King\'s Man Lvl 1',
+      checked: false
     },
     {
       number: 10,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 11,
       settlementEvent: '',
       story: 'Regal Visit',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 12,
       settlementEvent: '',
       story: 'Principle: Conviction',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 13,
       settlementEvent: '',
       story: '',
-      nemesis: 'The Hand Lvl 1'
+      nemesis: 'The Hand Lvl 1',
+      checked: false
     },
     {
       number: 14,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 15,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 16,
       settlementEvent: '',
       story: '',
-      nemesis: 'Butcher Lvl 2'
+      nemesis: 'Butcher Lvl 2',
+      checked: false
     },
     {
       number: 17,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 18,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 19,
       settlementEvent: '',
       story: '',
-      nemesis: 'King\'s Man Lvl 2'
+      nemesis: 'King\'s Man Lvl 2',
+      checked: false
     },
     {
       number: 20,
       settlementEvent: '',
       story: 'Watched',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 21,
@@ -130,55 +150,64 @@ export default {
       number: 22,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 23,
       settlementEvent: '',
       story: '',
-      nemesis: 'Butcher Lvl 3'
+      nemesis: 'Butcher Lvl 3',
+      checked: false
     },
     {
       number: 24,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 25,
       settlementEvent: '',
       story: '',
-      nemesis: 'Watcher'
+      nemesis: 'Watcher',
+      checked: false
     },
     {
       number: 26,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 27,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 28,
       settlementEvent: '',
       story: '',
-      nemesis: 'King\'s Man Lvl 3'
+      nemesis: 'King\'s Man Lvl 3',
+      checked: false
     },
     {
       number: 29,
       settlementEvent: '',
       story: '',
-      nemesis: ''
+      nemesis: '',
+      checked: false
     },
     {
       number: 30,
       settlementEvent: '',
       story: '',
-      nemesis: 'Gold Smoke Knight'
+      nemesis: 'Gold Smoke Knight',
+      checked: false
     }
   ]
 }
