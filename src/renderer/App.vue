fcd5380b35ece229c4723fbec827908aeaee7e83
Makenzie Schwartz <mawaschwa@gmail.com>
2018-06-06 19:12:21 -0400

    added fonts

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -18,6 +18,54 @@ export default {
 
 <style>
 /* Global CSS */
+@font-face {
+  font-family: "PT Sans";
+  src: url('~@/assets/fonts/PT_Sans/PT_Sans-Web-Regular.ttf')
+}
+
+@font-face {
+  font-family: "PT Sans";
+  font-weight: bold;
+  src: url('~@/assets/fonts/PT_Sans/PT_Sans-Web-Bold.ttf')
+}
+
+@font-face {
+  font-family: "PT Sans";
+  font-style: italic;
+  src: url('~@/assets/fonts/PT_Sans/PT_Sans-Web-Italic.ttf')
+}
+
+@font-face {
+  font-family: "PT Sans";
+  font-style: italic;
+  font-weight: bold;
+  src: url('~@/assets/fonts/PT_Sans/PT_Sans-Web-BoldItalic.ttf')
+}
+
+@font-face {
+  font-family: "Open Sans";
+  src: url('~@/assets/fonts/Open_Sans/OpenSans-Regular.ttf')
+}
+
+@font-face {
+  font-family: "Open Sans";
+  font-weight: bold;
+  src: url('~@/assets/fonts/Open_Sans/OpenSans-Bold.ttf')
+}
+
+@font-face {
+  font-family: "Open Sans";
+  font-style: italic;
+  src: url('~@/assets/fonts/Open_Sans/OpenSans-Italic.ttf')
+}
+
+@font-face {
+  font-family: "Open Sans";
+  font-weight: bold;
+  font-style: italic;
+  src: url('~@/assets/fonts/Open_Sans/OpenSans-BoldItalic.ttf')
+}
+
 .DISABLE-CLICKS-HISTORY-MODE {
   pointer-events: none;
 }
