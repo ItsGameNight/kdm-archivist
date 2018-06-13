bd06a184ea86353b47fc07ccdd0c717144f863e2
Makenzie Schwartz <mawaschwa@gmail.com>
2018-06-13 18:00:16 -0400

    added showdown stats and scrolling to BaseSurvivorModal

--- src/renderer/components/BaseSurvivorModal.vue
+++ src/renderer/components/BaseSurvivorModal.vue
@@ -1,105 +1,143 @@
 <template>
-  <modal class="BaseSurvivorModal" :modalWidth="'500'" @close="$emit('close')">
+  <modal class="BaseSurvivorModal" :modalWidth="'500'" noFooter @close="$emit('close')">
     <div slot="header">
       <h2 class="BaseSurvivorModal__title">Base Survivor Stats</h2>
       <h4 class="BaseSurvivorModal__subtitle">All new survivors inherit the properties of the settlement's base survivor.</h4>
     </div>
     <div slot="body" class="BaseSurvivorModal__body" :class="[themeClass]">
-      <h5 class="BaseSurvivorModal__sectionTitle">Hunt XP</h5>
-      <div class="BaseSurvivorModal__section">
-        <hunt-xp-bar
-          :level="currentSettlement.baseSurvivor.xp"
-          @onChange="update('xp', $event)" />
-        <p v-if="innovationUnlocked('saga')">+2 from the Saga innovation.</p>
-      </div>
-      <h5 class="BaseSurvivorModal__sectionTitle">Survival Actions</h5>
-      <div class="BaseSurvivorModal__section">
-        <div class="BaseSurvivorModal__survivalAction">
-          <square-toggle
-            :initValue="currentSettlement.baseSurvivor.dodge"
-            :statDisplayName="'Dodge'"
-            @update="update('dodge', $event)" />
-          <p>All survivors are born with this ability.</p>
-        </div>
-        <div class="BaseSurvivorModal__survivalAction">
-          <square-toggle
-            :initValue="currentSettlement.baseSurvivor.encourage"
-            :statDisplayName="'Encourage'"
-            @update="update('encourage', $event)" />
-          <p v-if="innovationUnlocked('language')">Unlocked by the Language innovation.</p>
-        </div>
-        <div class="BaseSurvivorModal__survivalAction">
-          <square-toggle
-            :initValue="currentSettlement.baseSurvivor.dash"
-            :statDisplayName="'Dash'"
-            @update="update('dash', $event)" />
-          <p v-if="innovationUnlocked('paint')">Unlocked by the Paint innovation.</p>
-        </div>
-        <div class="BaseSurvivorModal__survivalAction">
-          <square-toggle
-            :initValue="currentSettlement.baseSurvivor.surge"
-            :statDisplayName="'Surge'"
-            @update="update('surge', $event)" />
-          <p v-if="innovationUnlocked('inner lantern')">Unlocked by the Inner Lantern innovation.</p>
-        </div>
-        <div class="BaseSurvivorModal__survivalAction">
-          <square-toggle
-            :initValue="currentSettlement.baseSurvivor.endure"
-            :statDisplayName="'Endure'"
-            @update="update('endure', $event)" />
-          <p v-if="innovationUnlocked('destiny')">Unlocked by the Destiny innovation.</p>
-        </div>
-      </div>
-      <h5 class="BaseSurvivorModal__sectionTitle">Showdown Stats</h5>
-      <div class="BaseSurvivorModal__section">
-      </div>
-      <h5 class="BaseSurvivorModal__sectionTitle">Development Stats</h5>
-      <div class="BaseSurvivorModal__section">
-        <div class="BaseSurvivorModal__devWrapper first">
-          <weapon-proficiency-bar
-            :level="currentSettlement.baseSurvivor.weaponProficiencyLevel"
-            @onChange="update('weaponProficiencyLevel', $event)" />
-        </div>
-        <div class="BaseSurvivorModal__devWrapper">
-          <div class="BaseSurvivorModal__flexWrapper">
-            <courage-bar
-              :level="currentSettlement.baseSurvivor.courage"
-              @onChange="update('courage', $event)" />
-            <div class="BaseSurvivorModal__inputWrapper">
-              <editable-text-input
-                :textValue="currentSettlement.baseSurvivor.boldSkill"
-                :textStyle="{ fontSize: '10pt' }"
-                :placeholder="'Base Bold Skill'"
-                @update="update('boldSkill', $event)" />
-            </div>
-          </div>
+      <div class="BaseSurvivorModal__scrollWrapper">
+        <h5 class="BaseSurvivorModal__sectionTitle">Hunt XP</h5>
+        <div class="BaseSurvivorModal__section">
+          <hunt-xp-bar
+            :level="currentSettlement.baseSurvivor.xp"
+            @onChange="update('xp', $event)" />
           <p v-if="innovationUnlocked('saga')">+2 from the Saga innovation.</p>
         </div>
-        <div class="BaseSurvivorModal__devWrapper">
-          <div class="BaseSurvivorModal__flexWrapper">
-            <understanding-bar
-              :level="currentSettlement.baseSurvivor.understanding"
-              @onChange="update('understanding', $event)" />
-            <div class="BaseSurvivorModal__inputWrapper">
-              <editable-text-input
-                :textValue="currentSettlement.baseSurvivor.insightSkill"
-                :textStyle="{ fontSize: '10pt' }"
-                :placeholder="'Base Insight Skill'"
-                @update="update('insightSkill', $event)" />
-            </div>
+        <h5 class="BaseSurvivorModal__sectionTitle">Survival Actions</h5>
+        <div class="BaseSurvivorModal__section">
+          <div class="BaseSurvivorModal__survivalAction">
+            <square-toggle
+              :initValue="currentSettlement.baseSurvivor.dodge"
+              :statDisplayName="'Dodge'"
+              @update="update('dodge', $event)" />
+            <p>All survivors are born with this ability.</p>
           </div>
-          <p v-if="principleUnlocked('graves')">+1 from the Graves principle.</p>
-          <p v-if="innovationUnlocked('saga')">+2 from the Saga innovation.</p>
+          <div class="BaseSurvivorModal__survivalAction">
+            <square-toggle
+              :initValue="currentSettlement.baseSurvivor.encourage"
+              :statDisplayName="'Encourage'"
+              @update="update('encourage', $event)" />
+            <p v-if="innovationUnlocked('language')">Unlocked by the Language innovation.</p>
+          </div>
+          <div class="BaseSurvivorModal__survivalAction">
+            <square-toggle
+              :initValue="currentSettlement.baseSurvivor.dash"
+              :statDisplayName="'Dash'"
+              @update="update('dash', $event)" />
+            <p v-if="innovationUnlocked('paint')">Unlocked by the Paint innovation.</p>
+          </div>
+          <div class="BaseSurvivorModal__survivalAction">
+            <square-toggle
+              :initValue="currentSettlement.baseSurvivor.surge"
+              :statDisplayName="'Surge'"
+              @update="update('surge', $event)" />
+            <p v-if="innovationUnlocked('inner lantern')">Unlocked by the Inner Lantern innovation.</p>
+          </div>
+          <div class="BaseSurvivorModal__survivalAction">
+            <square-toggle
+              :initValue="currentSettlement.baseSurvivor.endure"
+              :statDisplayName="'Endure'"
+              @update="update('endure', $event)" />
+            <p v-if="innovationUnlocked('destiny')">Unlocked by the Destiny innovation.</p>
+          </div>
+        </div>
+        <h5 class="BaseSurvivorModal__sectionTitle">Showdown Stats</h5>
+        <div class="BaseSurvivorModal__section">
+          <div class="BaseSurvivorModal__showdownWrapper">
+            <editableStat
+              :initValue="currentSettlement.baseSurvivor.movement"
+              :statDisplayName="'MOV'"
+              @update="update('movement', $event)" />
+            <editableStat
+              :initValue="currentSettlement.baseSurvivor.accuracy"
+              :statDisplayName="'ACC'"
+              @update="update('accuracy', $event)" />
+            <editableStat
+              :initValue="currentSettlement.baseSurvivor.strength"
+              :statDisplayName="'STR'"
+              @update="update('strength', $event)" />
+            <editableStat
+              :initValue="currentSettlement.baseSurvivor.evasion"
+              :statDisplayName="'EVA'"
+              @update="update('evasion', $event)" />
+            <editableStat
+              :initValue="currentSettlement.baseSurvivor.luck"
+              :statDisplayName="'LCK'"
+              @update="update('luck', $event)" />
+            <editableStat
+              :initValue="currentSettlement.baseSurvivor.speed"
+              :statDisplayName="'SPD'"
+              @update="update('speed', $event)" />
+          </div>
+          <p v-if="principleUnlocked('barbaric')">+1 STR from the Barbaric principle.</p>
+          <p v-if="principleUnlocked('survival of the fittest')">+1 STR, +1 EVA from the Survival of the Fittest principle.</p>
+          <p v-if="innovationUnlocked('clan of death')">+1 ACC, +1 STR, +1 EVA from the Clan of Death innovation.</p>
+        </div>
+        <h5 class="BaseSurvivorModal__sectionTitle">Development Stats</h5>
+        <div class="BaseSurvivorModal__section">
+          <div class="BaseSurvivorModal__devWrapper first">
+            <weapon-proficiency-bar
+              :level="currentSettlement.baseSurvivor.weaponProficiencyLevel"
+              @onChange="update('weaponProficiencyLevel', $event)" />
+          </div>
+          <div class="BaseSurvivorModal__devWrapper">
+            <div class="BaseSurvivorModal__flexWrapper">
+              <courage-bar
+                :level="currentSettlement.baseSurvivor.courage"
+                @onChange="update('courage', $event)" />
+              <div class="BaseSurvivorModal__inputWrapper">
+                <editable-text-input
+                  :textValue="currentSettlement.baseSurvivor.boldSkill"
+                  :textStyle="{ fontSize: '10pt' }"
+                  :placeholder="'Base Bold Skill'"
+                  @update="update('boldSkill', $event)" />
+              </div>
+            </div>
+            <p v-if="innovationUnlocked('saga')">+2 from the Saga innovation.</p>
+          </div>
+          <div class="BaseSurvivorModal__devWrapper">
+            <div class="BaseSurvivorModal__flexWrapper">
+              <understanding-bar
+                :level="currentSettlement.baseSurvivor.understanding"
+                @onChange="update('understanding', $event)" />
+              <div class="BaseSurvivorModal__inputWrapper">
+                <editable-text-input
+                  :textValue="currentSettlement.baseSurvivor.insightSkill"
+                  :textStyle="{ fontSize: '10pt' }"
+                  :placeholder="'Base Insight Skill'"
+                  @update="update('insightSkill', $event)" />
+              </div>
+            </div>
+            <p v-if="principleUnlocked('graves')">+1 from the Graves principle.</p>
+            <p v-if="innovationUnlocked('saga')">+2 from the Saga innovation.</p>
+          </div>
+          <div class="BaseSurvivorModal__scrollPadding"></div>
         </div>
       </div>
+      <div class="BaseSurvivorModal__scrollFadeBottom"></div>
     </div>
+    <div slot="footer" style="display: none;"></div>
   </modal>
 </template>
 
 <script type="text/javascript">
 import { mapGetters, mapActions } from 'vuex'
 import Modal from '@/components/Modal'
-import { SquareToggle, EditableTextInput } from '@/components/GUIComponents'
+import {
+  SquareToggle,
+  EditableTextInput,
+  EditableStat
+} from '@/components/GUIComponents'
 import {
   HuntXpBar,
   CourageBar,
@@ -115,6 +153,7 @@ export default {
     Modal,
     SquareToggle,
     EditableTextInput,
+    EditableStat,
     HuntXpBar,
     CourageBar,
     UnderstandingBar,
@@ -166,8 +205,28 @@ export default {
   }
 
   &__body {
+    position: relative;
     padding: 4px 8px;
     border-radius: 10px;
+  }
+
+  &__scrollWrapper {
+    max-height: 440px;
+    overflow: auto;
+  }
+
+  &__scrollPadding {
+    height: 10px;
+  }
+
+  &__scrollFadeBottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 20px;
+    border-radius: 0 0 10px 10px;
+    background: linear-gradient(rgba(0,0,0,0), $light-bg);
   }
 
   &__sectionTitle {
@@ -197,6 +256,13 @@ export default {
     &.first {
       padding-top: 0;
     }
+  }
+
+  &__showdownWrapper {
+    display: flex;
+    flex-direction: row;
+    padding-left: 10px;
+    padding-bottom: 5px;
   }
 
   &__flexWrapper {
