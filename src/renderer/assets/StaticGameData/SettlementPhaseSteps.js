b7e18ca38163527728bdb67ce5695734c2503257
Makenzie Schwartz <mawaschwa@gmail.com>
2018-06-15 16:20:29 -0400

    added remaining settlement phase steps

--- src/renderer/assets/StaticGameData/SettlementPhaseSteps.js
+++ src/renderer/assets/StaticGameData/SettlementPhaseSteps.js
@@ -11,6 +11,34 @@ export default {
     {
       title: 'Gain Endeavors',
       tipText: 'Use tokens to keep track of endeavors.'
+    },
+    {
+      title: 'Update Timeline',
+      tipText: '- Draw a settlement card and resolve it.\n- Trigger story event and settlement events for this year.'
+    },
+    {
+      title: 'Check Milestones',
+      tipText: 'From this step on, trigger any milestone story\nevents when their conditions are satisfied.'
+    },
+    {
+      title: 'Develop',
+      tipText: 'Spend endeavor, innovate, etc.'
+    },
+    {
+      title: 'Prepare Departing',
+      tipText: '- Remove any gear from returning survivors.\n- Prepare new departing survivors.'
+    },
+    {
+      title: 'Special Showdown',
+      tipText: 'Skip this if no special showdown. After showdown,\nheal survivors, remove their tokens, and return to Step 5.'
+    },
+    {
+      title: 'Update Storage',
+      tipText: '- Record any unused gear and resources.\n- Archive cards (put back in box).'
+    },
+    {
+      title: 'Depart!',
+      tipText: 'Start the hunt/showdown phase!'
     }
   ]
 }
